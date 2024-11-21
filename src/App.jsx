612f62a9dc26d69421f803ef160c21e640c885a9
atss20 <adrian.sanchez.ceb@immune.institute>
2024-11-21 12:33:12 +0100

    Trabajo de clase 2 - III (Finalizado)

--- src/App.jsx
+++ src/App.jsx
@@ -1,61 +1,18 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-import SeleccionarAnimal from './components/GetAnimals'
+// Importa el componente SeleccionarAnimal desde el archivo GetAnimals.jsx
+import SeleccionarAnimal from './components/GetAnimals.jsx'
+// Importa el archivo de estilos CSS para la aplicación
 import './App.css'
 
+// Define el componente principal de la aplicación llamado App
 function App() {
-
   return(
+    // Devuelve un contenedor div
     <div>
+      // Renderiza el componente SeleccionarAnimal dentro del div
       <SeleccionarAnimal></SeleccionarAnimal>
     </div>
   )
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  // const [count, setCount] = useState(0)
-
-  // return (
-  //   <>
-  //     <div>
-  //       <a href="https://vite.dev" target="_blank">
-  //         <img src={viteLogo} className="logo" alt="Vite logo" />
-  //       </a>
-  //       <a href="https://react.dev" target="_blank">
-  //         <img src={reactLogo} className="logo react" alt="React logo" />
-  //       </a>
-  //     </div>
-  //     <h1>Vite + React</h1>
-  //     <div className="card">
-  //       <button onClick={() => setCount((count) => count + 1)}>
-  //         count is {count}
-  //       </button>
-  //       <p>
-  //         Edit <code>src/App.jsx</code> and save to test HMR
-  //       </p>
-  //     </div>
-  //     <p className="read-the-docs">
-  //       Click on the Vite and React logos to learn more
-  //     </p>
-  //   </>
-  // )
 }
 
+// Exporta el componente App como el componente predeterminado del módulo
 export default App
